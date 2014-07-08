35475e9d97a2d457ecc056657f0e0cd7c4c98f4d
Eugene Shubin <eugene.shubin@sumup.com>
2014-07-08 23:31:26 +0200

    performance tests

--- src/app_env_var_app.erl
+++ src/app_env_var_app.erl
@@ -18,16 +18,36 @@ start(_StartType, _StartArgs) ->
 stop(_State) ->
     ok.
 
+
+-define(NUMBER_OF_TEST_ROUNDS, 100000).
+
+test_performance(TestRounds, Module) ->
+    {Time, _} = timer:tc(
+        fun() ->
+            lists:foreach(
+                fun(_) ->
+                    Module:use_env_variable()
+                end,
+                lists:seq(1, TestRounds)
+            )
+        end
+    ),
+    ?debugFmt("~p : ~p", [Module, Time]).
+
 performance_test_() ->
     {
         setup,
         fun() ->
-            application:start(app_env_var)
+            application:start(app_env_var),
+            ?NUMBER_OF_TEST_ROUNDS
         end,
         fun(_) ->
             application:stop(app_env_var)
         end,
-        fun(_) ->
-            []
+        fun(TestRounds) ->
+            [
+                fun() -> test_performance(TestRounds, gs1) end,
+                fun() -> test_performance(TestRounds, gs2) end
+            ]
         end
     }.
